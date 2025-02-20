dd9f5dcf78deb15a5f057cc51de65082d373165c
Dxckrr <juandavidp1127@gmail.com>
2025-02-20 00:26:00 -0500

    init dashboard page
    search , carousel component

--- bounsic-front/src/app/features/dashboard/dashboard.component.ts
+++ bounsic-front/src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarAppComponent } from '@app/shared/components/navbar/navbar-app.component';
+import { SearchBarComponent } from './searchBar/searchBar.component';
+import { SongsCarouselComponent } from "./songsCarousel/songsCarousel.components";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarAppComponent],
+  imports: [NavbarAppComponent, SearchBarComponent, SongsCarouselComponent],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
---
+++ bounsic-front/src/app/features/dashboard/songsCarousel/songsCarousel.components.ts
@@ -0,0 +1,47 @@
+// search-filter.component.ts
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LucideAngularModule, ChevronRight, ChevronLeft } from 'lucide-angular';
+
+@Component({
+  selector: 'dashboard-songsCarousel',
+  templateUrl: 'songsCarousel.component.html',
+  imports: [CommonModule, LucideAngularModule],
+})
+export class SongsCarouselComponent {
+  readonly ChevronRight = ChevronRight;
+  readonly ChevronLeft = ChevronLeft;
+  public albums = [
+    {
+      "id": 1,
+      "title": "Like You Do",
+      "artist": "Joji",
+      "cover": "https://i.pinimg.com/736x/b6/e5/6e/b6e56e823610424007e409a658445338.jpg"
+    },
+    {
+      "id": 2,
+      "title": "Blinding Lights",
+      "artist": "The Weeknd",
+      "cover": "https://i.pinimg.com/736x/d8/2d/b1/d82db192bfa37d3a0ca8594bf22f018f.jpg"
+    },
+    {
+      "id": 3,
+      "title": "After Hours",
+      "artist": "The Weeknd",
+      "cover": "https://i.pinimg.com/736x/04/26/97/042697ed3bf0ad7876683527bb33bcbb.jpg"
+    },
+    {
+      "id": 4,
+      "title": "Good Days",
+      "artist": "SZA",
+      "cover": "https://i.pinimg.com/736x/60/6e/42/606e42c9c46dac97d8a318f4af23c92e.jpg "
+    },
+    {
+      "id": 5,
+      "title": "Maria",
+      "artist": "Blondie",
+      "cover": "https://i.pinimg.com/736x/cd/4f/15/cd4f1528fbe6d5c1e9b89012fd7394b4.jpg"
+    }
+  ]
+
+}
