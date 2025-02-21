766e335f6e5f83157bfa0a790757d0c43814e766
Duck <118633132+Dxckrr@users.noreply.github.com>
2025-02-20 21:34:23 -0500

    feat login in azure
    first login attempt

--- bounsic-front/src/app/app.config.ts
+++ bounsic-front/src/app/app.config.ts
@@ -3,13 +3,36 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, HTTP_INTERCEPTORS, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+// MSAL functions
+import { MSAL_INSTANCE, MsalInterceptor, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG, MsalService, MsalGuard, MsalBroadcastService } from '@azure/msal-angular';
+import { MSALInstanceFactory, MSALGuardConfigFactory, MSALInterceptorConfigFactory } from './config/msal.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch()),
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
   ]
 };
---
+++ bounsic-front/src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    msalConfig: {
+        auth: {
+            clientId: '69070e99-af46-49f0-a4f4-228ac2f8b61f',
+            authority: 'https://login.microsoftonline.com/af2aa024-2160-4e82-a754-865b614d0ff9'
+        }
+    },
+    apiConfig: {
+        scopes: ['user.read'],
+        uri: 'https://graph.microsoft.com/v1.0/me'
+    }
+};
