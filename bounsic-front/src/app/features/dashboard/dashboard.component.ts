8727ea6ec5a3dcc628da08d387b336cc5fe9b863
Duck <118633132+Dxckrr@users.noreply.github.com>
2025-03-21 17:28:17 -0500

    init trending songs

--- bounsic-front/src/app/features/dashboard/dashboard.component.ts
+++ bounsic-front/src/app/features/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { SearchBarComponent } from './searchBar/search-bar.component';
 import { SongsCarouselComponent } from './songsCarousel/songs-carousel.component';
 import { RouterModule } from '@angular/router';
 import { SafeChoiceListComponent } from './safeChoicesList/safe-choices-list.component';
+import { TrendingSongsComponent } from "./trendingSongs/trending-songs.component";
 
 @Component({
   selector: 'app-dashboard',
@@ -15,7 +16,8 @@ import { SafeChoiceListComponent } from './safeChoicesList/safe-choices-list.com
     RouterModule,
     SearchBarComponent,
     SafeChoiceListComponent,
-  ],
+    TrendingSongsComponent
+],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
