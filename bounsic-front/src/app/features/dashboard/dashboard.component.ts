d6563af705728ebad8d3bd0ab7717f6a700443ae
Duck <118633132+Dxckrr@users.noreply.github.com>
2025-03-21 11:01:53 -0500

    update dashboard & responsive ui

--- bounsic-front/src/app/features/dashboard/dashboard.component.ts
+++ bounsic-front/src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarAppComponent } from '@app/shared/components/navbar/navbar-app.component';
 import { SearchBarComponent } from './searchBar/search-bar.component';
-import { SongsCarouselComponent } from "./songsCarousel/songs-carousel.component";
+import { SongsCarouselComponent } from './songsCarousel/songs-carousel.component';
 import { RouterModule } from '@angular/router';
+import { SafeChoiceListComponent } from './safeChoicesList/safe-choices-list.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarAppComponent, SearchBarComponent, SongsCarouselComponent, RouterModule],
+  imports: [
+    NavbarAppComponent,
+    SearchBarComponent,
+    SongsCarouselComponent,
+    RouterModule,
+    SearchBarComponent,
+    SafeChoiceListComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent { }
-
+export class DashboardComponent {}
