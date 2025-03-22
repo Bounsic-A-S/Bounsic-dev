b2304fb14fb870d20bc7a2eb8773918b68d5c4fb
Dxckrr <juandavidp1127@gmail.com>
2025-03-22 14:21:06 -0500

    feat generate sas for img

--- bounsic-front/src/app/features/player/player.component.ts
+++ bounsic-front/src/app/features/player/player.component.ts
@@ -11,4 +11,8 @@ import { SongListComponent } from "./info/song_list/song-list.component";
   templateUrl: './player.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlayerComponent { }
+export class PlayerComponent { 
+  public song = {
+    mp3:"https://bounsic1imgs.blob.core.windows.net/bounsic-songs-imgs/Adele%20-%20Rolling%20in%20the%20Deep%20(Official%20Music%20Video).mp3?sp=r&st=2025-03-22T18:54:02Z&se=2025-03-23T02:54:02Z&spr=https&sv=2024-11-04&sr=b&sig=AUMA7DV6aAHGaux9MyYvWaxsWHa%2F1HmEw9I8359duuI%3D"
+  }
+}
