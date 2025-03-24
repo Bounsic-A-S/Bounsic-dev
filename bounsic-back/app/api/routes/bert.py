c567ad4f85a9d23c29eaeed57e088570670ede8a
Dxckrr <juandavidp1127@gmail.com>
2025-03-23 20:21:14 -0500

    init mongo connection

--- bounsic-back/app/api/routes/bert.py
+++ bounsic-back/app/api/routes/bert.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from app.services import pregunta_respuesta
+from app.provider.mongo_db import db
 
 router = APIRouter()
 
 @router.get("/")
 async def home():
+    try:
+        print("Conexión exitosa a MongoDB:", db.list_collection_names())
+    except Exception as e:
+        print("Error conectando a MongoDB:", e)
     return {"message": "¡Hola, Bounsic-back está funcionando!"}
 
 @router.post("/ask")
