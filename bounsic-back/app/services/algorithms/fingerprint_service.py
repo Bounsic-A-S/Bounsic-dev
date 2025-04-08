66f8c0f2810e892eab9820f87351b4b6cb1eb1ff
JCarvajalr <juandacr25@gmail.com>
2025-04-07 21:33:58 -0500

    new fingerprint version

--- bounsic-back/app/services/algorithms/fingerprint_service.py
+++ bounsic-back/app/services/algorithms/fingerprint_service.py
@@ -7,52 +7,109 @@ from scipy.signal import find_peaks
 # from azure.storage.blob import BlobServiceClient
 # from app.provider import db
 
-def generate_fingerprint(songName: str):
+
+def generate_fingerprint(songName: str, segment_duration: float = 0.5, top_n_freqs: int = 1):
     try:
-        archivo_audio = os.path.join("app", "services", "songs", f"{songName}.mp3")
-        songName = os.path.splitext(os.path.basename(archivo_audio))[0]
-
-        # Read audio
-        y, sr = librosa.load(archivo_audio, sr=None)
+        songName = songName.lower()
+        song_path = os.path.join("app", "services", "songs", f"{songName}.mp3")
+        if not os.path.exists(song_path):
+            raise FileNotFoundError(f"Audio file not found: {song_path}")
         
-        D = np.abs(librosa.stft(y))
-        D_dB = librosa.amplitude_to_db(D, ref=np.max)
 
-        frequencies = librosa.fft_frequencies(sr=sr) # Get frequencies in Hz
-        fingerprint = _analyze_frequencies(D_dB=D_dB, frequencies=frequencies)
+        # Read and normalize audio
+        y, sr = librosa.load(song_path, sr=None)
+        y = librosa.util.normalize(y)
+        bpm, _ = librosa.beat.beat_track(y=y, sr=sr)
+
+        print(f"sr: {sr}")
+        print(f"BPM: {bpm}")
+
+        # Calculate STFT
+        n_fft = 2048  # More frequency bins
+        hop_length = n_fft // 4
+        D = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length))
+        # Frecuencia  1    2    3    4    5    ...   n_frames
+        # ---------------------------------------------------
+        # 0 Hz       [dB1, dB2, dB3, dB4, dB5, ...]  
+        # 100 Hz     [dB1, dB2, dB3, dB4, dB5, ...]  
+        # 200 Hz     [dB1, dB2, dB3, dB4, dB5, ...]  
+        # ...        ...  ...  ...  ...  ...  ...   
+        # sr/2       [dB1, dB2, dB3, dB4, dB5, ...]  
+        print(f"max: {np.max(D)}")
+        # D_normalized = D / np.max(D)  # Normaliza en escala lineal
+        D_dB = librosa.amplitude_to_db(D, ref=0)
+        
+        # Segment size
+        frames_per_segment = int(segment_duration * sr / hop_length)
+        
+        frequencies = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
+
+        # for i, freq in enumerate(frequencies[:500]): # Max frequencie: 240000.00 Hz   
+        #     print(f"Fila {i} corresponde a {freq:.2f} Hz")
+
+        print(f"Frames per segment: {frames_per_segment}")
+        print(f"Top n frequencies: {top_n_freqs}")
+        bpm = float(np.array(bpm).item())
+        fingerprint = {
+            "bpm": float(round(bpm, 2)),
+            "frequencies": _analyze_frequencies(
+                D_dB=D_dB, 
+                frequencies=frequencies,
+                frames_per_segment=frames_per_segment,
+                top_n_freqs=top_n_freqs,
+                hop_length=hop_length,
+                sr=sr
+            )
+        }
 
         save_json(fingerprint=fingerprint, songName=songName)
         return True
-    except:
-        print("Error generating fingerprint.")
+        
+    except FileNotFoundError as e:
+        print(f"File error: {str(e)}")
+        return False
+    except Exception as e:
+        print(f"Error generating fingerprint: {str(e)}")
         return False
 
-def _analyze_frequencies(D_dB, frequencies):
+def _analyze_frequencies(D_dB, frequencies, frames_per_segment, top_n_freqs, hop_length, sr):
     fingerprint = []
-
-    # Iterar sobre cada fragmento amplio del espectrograma
-    for t in range(0, D_dB.shape[1], 100):  # 100 windows
-        espectro_tiempo = D_dB[:, t]  # Espectro en el tiempo t
-        picos, _ = find_peaks(espectro_tiempo, height=np.percentile(espectro_tiempo, 97))
+    max_global = np.max(D_dB)
+    
+    for t in range(0, D_dB.shape[1], frames_per_segment):
+        segment = D_dB[:, t:t+(frames_per_segment-1)]
+        # Average spectrum over the segment duration
+        avg_spectrum = np.mean(segment, axis=1)
         
-        # Save only 2 frequencies per time fragment
-        if len(picos) > 0:
-            top_frequencies = sorted(picos, key=lambda p: espectro_tiempo[p], reverse=True)[:2]
-            fingerprint.append([round(frequencies[p], 1) for p in top_frequencies])
+        # Find prominent peaks
+        pks, _ = find_peaks(
+            avg_spectrum, 
+            height=np.percentile(avg_spectrum, 95),
+            distance=10  # Minimum distance between peaks
+        )
+        avg_spectrum_norm = (avg_spectrum / max_global) * 100
 
+        if len(pks) > 0:
+            # Get top N frequencies by amplitude
+            top_freqs = sorted(pks, key=lambda p: avg_spectrum[p], reverse=True)[:top_n_freqs]
+            fingerprint.append({
+            'time': float(round(t * hop_length   / sr, 3)),
+            'frequencies': float(round(frequencies[top_freqs[0]], 1)),
+            'amplitudes': float("{:.2f}".format(avg_spectrum_norm[top_freqs[0]]))
+        })
     return fingerprint
 
 def save_json(fingerprint, songName: str):
-    saveIn = "fingerprints"
-    os.makedirs(saveIn, exist_ok=True)
 
-    ruta_json = os.path.join("app", "services", saveIn, f"{songName}.json")
+    ruta_json = os.path.join("app", "services", "fingerprints", f"{songName}.json")
     with open(ruta_json, "w", encoding="utf-8") as f:
         json.dump(fingerprint, f, indent=4)
 
+    # Use json.dumps 
     print(f"✅ Huella digital guardada en: {ruta_json}")
 
-def readFingerprint(songName: str) -> list[tuple[float, float]]:
+def readFingerprint(songName: str) -> list[tuple[float, float]]: # Fix to new fingerprint
+    songName = songName.lower()
     ruta_json = os.path.join("app", "services", "fingerprints", f"{songName}.json")
     
     with open(ruta_json, "r", encoding="utf-8") as file:
@@ -60,13 +117,10 @@ def readFingerprint(songName: str) -> list[tuple[float, float]]:
 
     frequency_pairs = [tuple(pair) for pair in fingerprint_data]
 
-    # print("Primeros pares de frecuencias:")
-    # for pair in frequency_pairs[:5]:
-    #     print(f"F1: {pair[0]} Hz, F2: {pair[1]} Hz")
     return frequency_pairs
 
-# def main():
-#     # generate_fingerprint()
-#     readFingerprint()
-# if __name__ == "__main__":
-#     main()
+def main():
+    generate_fingerprint("oblivion")
+    
+if __name__ == "__main__":
+    main()
--- bounsic-back/app/services/algorithms/rhythm_service.py
+++ bounsic-back/app/services/algorithms/rhythm_service.py
@@ -1,3 +1,27 @@
+import matplotlib.pyplot as plt
+from typing import List, Tuple
+# from app.services.algorithms.fingerprint_service import readFingerprint
+
+from fingerprint_service import generate_fingerprint, readFingerprint
+
+def graph_frequencies(song_name: str, artist: str, usar_segundo_valor=False):
+    """
+    Grafica los valores de una lista de tuplas.
+    
+    :param datos: Lista de tuplas (float, float)
+    :param usar_segundo_valor: Si True, grafica el segundo valor de cada tupla, si False el primero.
+    """
+    datos = readFingerprint(song_name)
+    x = list(range(len(datos)))  # Índices como eje X
+    y = [valor[1] if usar_segundo_valor else valor[0] for valor in datos]
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(x, y, marker='o', linestyle='-', color='b')
+    plt.xlabel('')
+    plt.ylabel('Frequencie value')
+    plt.title(f'{song_name} - ({artist})')
+    plt.grid(True)
+    plt.show()
 
 # Algorithm to get alike songs based on their sound
 def get_alikes(songName: str):
@@ -5,3 +29,13 @@ def get_alikes(songName: str):
 
 
     return 0
+
+
+def main():
+    song_name = "Genesis"
+    artist = "Grimes"
+    generate_fingerprint(song_name)
+    graph_frequencies(song_name, artist)
+
+if __name__ == "__main__":
+    main()
