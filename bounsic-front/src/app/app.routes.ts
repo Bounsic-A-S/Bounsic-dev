24ccab6e729612396a277f82c84f5ea310a9fd78
Dxckrr <juandavidp1127@gmail.com>
2025-04-10 01:27:05 -0500

    init child routes on setting-user

--- bounsic-front/src/app/app.routes.ts
+++ bounsic-front/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { LibraryComponent } from './features/library/library.component';
 import { PlaylistComponent } from './features/playlist/playlist.component';
 import { NotFoundComponent } from './features/404/404.component';
 import { SettingsComponent } from './features/user/settings/setting.component';
+import { SettingsAccountComponent } from './features/user/settings/account/account.component';
+import { SettingsAppearanceComponent } from './features/user/settings/color/appearance.component';
 
 
 
@@ -29,6 +31,17 @@ export const routes: Routes = [
     {
         path:'settings',
         component: SettingsComponent,
+        children: [
+            { path: '', redirectTo: 'account', pathMatch: 'full' },
+            { path: 'account', component: SettingsAccountComponent },
+            // { path: 'privacy', component: PrivacyComponent },
+            // { path: 'connections', component: ConnectionsComponent },
+            // { path: 'notifications', component: NotificationsComponent },
+            // { path: 'plan', component: PlanComponent },
+            // { path: 'playback', component: PlaybackComponent },
+            { path: 'appearance', component: SettingsAppearanceComponent },
+            // { path: 'language', component: LanguageComponent }
+          ]
     },
     {
         path: 'playlist/:id',
--- bounsic-front/src/app/features/user/settings/account/account.component.ts
+++ bounsic-front/src/app/features/user/settings/account/account.component.ts
@@ -1,14 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 @Component({
-  selector: 'app-user-settings',
+  selector: 'user-settings-account',
   standalone: true,
   imports: [
-    CommonModule,RouterModule,
+    CommonModule,
   ],
-  templateUrl: './settings.component.html',
+  templateUrl: './account.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsComponent {
+export class SettingsAccountComponent {
 }
