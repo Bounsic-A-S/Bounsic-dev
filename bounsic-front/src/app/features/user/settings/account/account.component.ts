25c26d3009b9fd0175fd24b701fa7122e45825f0
Dxckrr <juandavidp1127@gmail.com>
2025-04-15 18:50:06 -0500

    add more texts & refactor .jsons

--- bounsic-front/src/app/features/user/settings/account/account.component.ts
+++ bounsic-front/src/app/features/user/settings/account/account.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'user-settings-account',
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule
   ],
   templateUrl: './account.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
