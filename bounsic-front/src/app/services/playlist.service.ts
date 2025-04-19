9a674623cf8f6ad1c1b2a03f8c9c0da7828b39ad
Dxckrr <juandavidp1127@gmail.com>
2025-04-19 09:57:21 -0500

    update services logic & component render
    there's a  weird mistake on getplaylist by id

--- bounsic-front/src/app/services/playlist.service.ts
+++ bounsic-front/src/app/services/playlist.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, switchMap, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment.dev';
 
 @Injectable({
@@ -8,12 +9,37 @@ import { environment } from '../../environments/environment.dev';
 })
 export class PlaylistService {
   private apiUrl = environment.apiUrl;
+  private playlistsSubject = new BehaviorSubject<any[]>([]);
+  private playlists$ = this.playlistsSubject.asObservable();
 
   constructor(private http: HttpClient) {}
-  getAllPlaylist(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/playlist/all`);
+
+  getAllPlaylist(): Observable<any[]> {
+    if (this.playlistsSubject.value.length > 0) {
+      return this.playlists$;
+    }
+
+    return this.http.get<any[]>(`${this.apiUrl}/playlist/all`).pipe(
+      tap((playlists) => {
+        this.playlistsSubject.next(playlists);
+      }),
+      catchError((err) => {
+        console.error('Error al obtener playlists:', err);
+        return of([]);
+      })
+    );
   }
-  getPlaylistById(id:string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/playlist/${id}`);
+
+  getPlaylistById(playlistId: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/playlist/${playlistId}`).pipe(
+      catchError((err) => {
+        console.error('Error al obtener playlist:', err);
+        return of(null);
+      })
+    );
+  }
+
+  refreshPlaylists(): void {
+    this.getAllPlaylist().subscribe();
   }
 }
