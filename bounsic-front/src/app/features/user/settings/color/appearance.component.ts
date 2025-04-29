8b8f8ad27b0cc1d0cca571d78adbac91635f5908
Dxckrr <juandavidp1127@gmail.com>
2025-04-29 11:10:13 -0500

    init background customization

--- bounsic-front/src/app/features/user/settings/color/appearance.component.ts
+++ bounsic-front/src/app/features/user/settings/color/appearance.component.ts
@@ -12,4 +12,37 @@ import { TranslateModule } from '@ngx-translate/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsAppearanceComponent {
+  private theme : string = 'dark';
+  public customThemeSelected: string = localStorage.getItem('background') ?? 'bg-bounsic-gradient';
+  public customThemes = [
+    {
+      bounsic: [
+        { name: 'bg-bounsic-gradient', isLight: false },
+        { name: 'bg-bounsic-custom-gradient-blue', isLight: false },
+        { name: 'bg-bounsic-custom-gradient-purple', isLight: false },
+      ],
+      gradient: [
+        { name: 'bg-bounsic-gradient-about-mobile', isLight: false }
+      ],
+      solid: [
+        { name: 'bg-bounsic-red', isLight: true },
+        { name: 'bg-bounsic-yellow', isLight: true }
+
+      ]
+    }
+  ];
+  
+  toggleTheme(theme: string): void {
+    this.theme = theme;
+  }
+
+  private getTheme(): string {
+    return this.theme;
+  }
+  isLightTheme(): boolean {
+    return this.getTheme() === 'light';
+  }
+  setCustomTheme(theme: string): void {
+    this.customThemeSelected = theme;
+  }
 }
