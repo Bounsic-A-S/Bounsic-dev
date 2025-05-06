07fbaa04be901ee18abe207f89b9bf6cdd2d31c4
Chris <128336431+ChisCurubo@users.noreply.github.com>
2025-05-05 20:59:39 -0500

    feat/404 done
    done with this shit, i hate carvalal

--- bounsic-front/src/app/features/error/button/404_button.component.ts
+++ bounsic-front/src/app/features/error/button/404_button.component.ts
@@ -5,6 +5,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
     selector: 'router-err-button',
     standalone: true,
     templateUrl: './404_button.component.html',
+    styleUrl: './404_button.component.css',
     imports: [CommonModule],
     changeDetection: ChangeDetectionStrategy.OnPush
 
