692a3df3fb152e3f8f59793313701b7477ab3b0f
Dxckrr <juandavidp1127@gmail.com>
2025-05-08 00:36:43 -0500

    update custom bgs

--- bounsic-front/src/environments/environment.dev.ts
+++ bounsic-front/src/environments/environment.dev.ts
@@ -1,7 +1,11 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:1801', //back //http://20.81.217.99:1801
-    api_S_Url: 'http://localhost:4000',
+    // apiUrl: 'http://localhost:1801', //back
+    // api_S_Url: 'http://localhost:4000', //stream
+
+    apiUrl: 'http://20.81.217.99:1801', //back // http://20.81.217.99:1801
+    api_S_Url: 'https://streaming-cchyanf7gdhch9az.eastus2-01.azurewebsites.net', //streaming // https://streaming-cchyanf7gdhch9az.eastus2-01.azurewebsites.net
+
     msalConfig: {
         auth: {
             clientId: '358ef62a-08c7-4755-9a61-78f1ebd0cd49',
