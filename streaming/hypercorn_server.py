9b84f57b5ded948a4e5b9289e7e781a06d0f7c78
Chris <128336431+ChisCurubo@users.noreply.github.com>
2025-05-07 22:31:42 -0500

    new try of docker streaming

--- streaming/hypercorn_server.py
+++ streaming/hypercorn_server.py
@@ -6,7 +6,7 @@ from main import app  # Asegúrate de que 'main.py' tiene tu app FastAPI
 async def run():
     # Configurar Hypercorn sin SSL (HTTP/2 sin cifrado solo es válido para desarrollo local)
     config = Config()
-    config.bind = ["0.0.0.0:4000"]
+    config.bind = ["0.0.0.0:4000"]  # Cambiado a 0.0.0.0 para aceptar conexiones externas
     config.workers = 2
     config.alpn_protocols = ["h2"]  # Activa HTTP/2
     config.use_http2 = True  # Habilita HTTP/2
